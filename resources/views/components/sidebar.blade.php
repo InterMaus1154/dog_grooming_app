499483fd680f2fb6ec691ff4aee94d04574623ee
intermaus1154 <amarco1154@gmail.com>
2026-04-24 19:50:16 +0100

    finish sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,12 +2,13 @@
        class=" shrink-0 bg-brand-light text-black transition-all duration-300 h-full relative">
     <div class="p-4 flex flex-col gap-4">
         <h1 class="text-4xl text-center font-bold italic">
-            <a href="/">GabiGrooming</a>
+            <a href="{{route('dashboard')}}">GabiGrooming</a>
         </h1>
+        <h2 class="text-center text-xl">Hi, <i>{{auth()->user()->username}}</i></h2>
         <nav>
             <ul class="flex flex-col gap-2">
                 <li>
-                    <a href="/" class="bg-white block text-center text-xl hover:bg-brand-dark hover:text-white transition-all duration-200 font-bold rounded-md py-1 cursor-pointer">Home</a>
+                    <a href="{{route('dashboard')}}" class="bg-white block text-center text-xl hover:bg-brand-dark hover:text-white transition-all duration-200 font-bold rounded-md py-1 cursor-pointer">Home</a>
                 </li>
                 <li>
                     <a class="bg-white block text-center text-xl hover:bg-brand-dark hover:text-white transition-all duration-200 font-bold rounded-md py-1 cursor-pointer">Dogs</a>
