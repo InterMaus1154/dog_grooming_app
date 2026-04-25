9d12ffb56acf95fccb255c06f5f3464d72e87ab9
intermaus1154 <amarco1154@gmail.com>
2026-04-25 09:34:07 +0100

    change login page size

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
             <a href="https://markkweb.net/" target="_blank" class="underline text-blue-400">MK</a>
         </h1>
     </div>
-    <div class="h-screen p-4 sm:p-0 flex items-center sm:justify-center">
+    <div class="h-svh p-4 sm:p-0 flex items-center sm:justify-center">
         <div
             class="p-8 w-full sm:w-auto sm:min-w-[25%] border border-gray-200 rounded-md flex flex-col text-center gap-8 bg-white">
             <h1 class="text-4xl text-brand font-bold">Login</h1>
