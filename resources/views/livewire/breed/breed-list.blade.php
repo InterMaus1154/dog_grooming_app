f444a4fd96eeeff21db034c8a2fae558083cc36f
Mark <amarco1154@gmail.com>
2026-04-28 16:32:18 +0100

    change debounce vlaue

--- resources/views/livewire/breed/breed-list.blade.php
+++ resources/views/livewire/breed/breed-list.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-4">
     {{--filters--}}
     <form wire:submit.prevent class="w-max md:w-[25%] space-y-2 flex items-center justify-center gap-2 ">
-        <x-form.input type="text" placeholder="Search by name..." wire:model.live.debounce.500ms="filters.name"/>
+        <x-form.input type="text" placeholder="Search by name..." wire:model.live.debounce.300ms="filters.name"/>
         <x-button class="bg-brand!" wire:click="clearFilters()">Reset</x-button>
     </form>
 
