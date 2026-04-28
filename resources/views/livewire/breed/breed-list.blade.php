dcdeafb3ec74aa4ca0426850c4ca8cd130254cc9
Mark <amarco1154@gmail.com>
2026-04-28 21:42:55 +0100

    add found to filter

--- resources/views/livewire/breed/breed-list.blade.php
+++ resources/views/livewire/breed/breed-list.blade.php
@@ -1,8 +1,13 @@
 <div class="space-y-4">
     {{--filters--}}
-    <form wire:submit.prevent class="w-max md:w-[25%] space-y-2 flex items-center justify-center gap-2 ">
+    <form wire:submit.prevent class="md:w-[30%] space-y-2 flex items-center justify-center gap-2">
         <x-form.input type="text" placeholder="Search by name..." wire:model.live.debounce.300ms="filters.name"/>
         <x-button class="bg-brand!" wire:click="clearFilters()">Reset</x-button>
+        <span class="text-sm text-brand-dark whitespace-nowrap">
+            @if(!empty($filters['name']))
+                Found: {{$breeds->total()}}
+            @endif
+        </span>
     </form>
 
     <x-table>
