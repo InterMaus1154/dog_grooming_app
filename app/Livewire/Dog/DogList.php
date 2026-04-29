3eccf77b65ef6eee7b9ae400be7656fdd22b03c1
Mark <amarco1154@gmail.com>
2026-04-29 18:13:55 +0100

    add dog bases

---
+++ app/Livewire/Dog/DogList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dog;
+
+use Livewire\Component;
+
+class DogList extends Component
+{
+    public function render()
+    {
+        return view('livewire.dog.dog-list');
+    }
+}
