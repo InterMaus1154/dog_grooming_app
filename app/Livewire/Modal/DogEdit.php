2a87ebec50cc2dea614ad7a19c87ea062702d07a
Mark <amarco1154@gmail.com>
2026-04-29 19:59:04 +0100

    update dog

--- app/Livewire/Modal/DogEdit.php
+++ app/Livewire/Modal/DogEdit.php
@@ -2,12 +2,66 @@
 
 namespace App\Livewire\Modal;
 
+use App\Livewire\Forms\DogForm;
+use App\Models\Customer;
+use App\Models\Dog;
+use App\Models\DogBreed;
+use Illuminate\View\View;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use WireUi\Traits\WireUiActions;
 
 class DogEdit extends Component
 {
-    public function render()
+
+    use WireUiActions;
+
+    public DogForm $form;
+    public Dog $dog;
+
+    public function mount(int $id): void
     {
-        return view('livewire.modal.dog-edit');
+        $this->dog = Dog::findOrFail($id);
+        $this->form->name = $this->dog->name;
+        $this->form->breedId = $this->dog->dog_breed_id;
+        $this->form->customerId = $this->dog->customer_id;
+    }
+
+
+    #[On('refresh-customers')]
+    #[On('refresh-breeds')]
+    public function eventOnRefresh(): void
+    {
+    }
+
+    public function save(): void
+    {
+        $this->form->validate();
+
+        try {
+            $this->dog->update([
+                'name' => $this->form->name,
+                'dog_breed_id' => $this->form->breedId,
+                'customer_id' => $this->form->customerId
+            ]);
+            $this->notification()->success('Dog successfully updated');
+        } catch (\Exception $e) {
+            $this->notification()->error('Error at updating dog');
+            activity('dog')
+                ->withProperties(['e' => $e->getMessage()])
+                ->on($this->dog)
+                ->log('Error at updating a dog record');
+        }
+
+        $this->dispatch('modal-close');
+        $this->dispatch('refresh-dogs');
+
+    }
+
+    public function render(): View
+    {
+        $customers = Customer::query()->orderBy('name')->get();
+        $breeds = DogBreed::all();
+        return view('livewire.modal.dog-edit', compact('customers', 'breeds'));
     }
 }
--- resources/views/livewire/modal/dog-create.blade.php
+++ resources/views/livewire/modal/dog-create.blade.php
@@ -10,7 +10,8 @@
                 <x-form.wrapper class="w-full">
                     <x-form.label for="customer_id" class="relative">Customer
                         <span class="text-red-500">*</span>
-                        <x-mini-button class="absolute top-[-25%]" rounded icon="plus" flat rose @click="$dispatch('modal-open', {component: 'modal.customer-create'})"/>
+                        <x-mini-button class="absolute top-[-25%]" rounded icon="plus" flat rose
+                                       @click="$dispatch('modal-open', {component: 'modal.customer-create'})"/>
                     </x-form.label>
                     <x-select id="customer_id" placeholder="Select a customer" :min-items-for-search="0"
                               :options="$customers" option-label="name" option-value="id"
@@ -22,7 +23,8 @@
                 <x-form.wrapper class="w-full">
                     <x-form.label for="breed_id" class="relative">Breed
                         <span class="text-red-500">*</span>
-                        <x-mini-button class="absolute top-[-25%]" rounded icon="plus" flat rose @click="$dispatch('modal-open', {component: 'modal.dog-breed-create'})"/>
+                        <x-mini-button class="absolute top-[-25%]" rounded icon="plus" flat rose
+                                       @click="$dispatch('modal-open', {component: 'modal.dog-breed-create'})"/>
                     </x-form.label>
                     <x-select id="breed_id" placeholder="Select a breed" :min-items-for-search="0"
                               :options="$breeds" option-label="name" option-value="id"
