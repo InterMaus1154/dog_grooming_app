5b5b4b0fde5b179e4480095ad5950d47a13e100e
intermaus1154 <amarco1154@gmail.com>
2026-05-02 20:25:11 +0100

    change on dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,4 @@
 <x-app title="Dashboard" headerTitle="Dashboard">
     <livewire:booking.booking-calendar />
-    <p class="italic mt-4">Click on a day to create a booking</p>
+    <p class="italic mt-4 text-right">Click on a slot to create a booking</p>
 </x-app>
